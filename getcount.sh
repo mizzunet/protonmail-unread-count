28150f4f9a04a553e8910cbd7a75d911b63f581d
Missu <mizzunet@protonmail.com>
2022-05-18 17:26:12 +0530

    hi

---
+++ getcount.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p ~/.config/config/hydroxide
+echo \{\"$USER\":\"$AUTH\"\} > ~/.config/hydroxide/auth.json
+
+hydroxide server &> /dev/null &
+sleep 2
+unread-count -u mizzunet -p $HYDROXIDE_BRIDGE_PASS -S 127.0.0.1 -P 1143
